7752d09c848e04b06b4aadfd02371fb6ab0b7e23
Ismael Ramos <ramosdumas_ismael@hotmail.com>
2025-10-02 04:43:44 -0600

    Middleware para manejo de api key agregado

--- service-data-access-layer/src/app.module.ts
+++ service-data-access-layer/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { appConfig } from './config/app.config';
 import { ClientModule } from './client/client.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ApiKeyMiddleware } from './middlewares/api-key/api-key.middleware';
 
 @Module({
   imports: [
@@ -19,4 +20,10 @@ import { PrismaModule } from './prisma/prisma.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ApiKeyMiddleware)
+      .forRoutes('*');
+  }
+}
